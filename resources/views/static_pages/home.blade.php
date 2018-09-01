d9f85250a2583bfc15b1a8d7c08c0e6b3a7e69ca
Zero <s812419396@gmail.com>
2018-09-01 02:08:52 +0000

    Add user statuses

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-<div class="jumbotron">
-    <h1>欢迎来到我的世界</h1>
-    <p>To Do</p>
-    <a href="{{ route('signup') }}" class="btn btn-primary btn-lg" role="button">现在注册</a>
-</div>
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+                <h3>微博列表</h3>
+                @include('shared._feed')
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>欢迎来到我的世界</h1>
+            <p>To Do</p>
+            <a href="{{ route('signup') }}" class="btn btn-primary btn-lg" role="button">现在注册</a>
+        </div>
+    @endif
 @stop
